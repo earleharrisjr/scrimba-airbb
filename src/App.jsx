5c928ba30564a74f0534320914f80662ce43fd5f
Earl Harris <earleharrisjr@gmail.com>
2022-11-30 16:22:46 -0500

    create hero component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Hero() {
+    return (
+        <section className="hero--component">
+            <img src="public/photo-grid.png" className="photo--grid" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--words">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
