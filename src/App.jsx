13892da37381ba3e54137681b9d94faf389ded53
Earl Harris <earleharrisjr@gmail.com>
2022-11-30 15:46:46 -0500

    style nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-<p>test</p>
+    <div>
+      <NavBar />
+    </div>
   )
 }
 
