b1a8b6b2cee308005c8a3c5dcab526f2adf95879
Earl Harris <earleharrisjr@gmail.com>
2022-12-01 11:17:50 -0500

    create card comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <div>
       <NavBar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card />
     </div>
   )
 }
