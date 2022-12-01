0944d279904bf26e7918351450033cc3e879c5cf
Earl Harris <earleharrisjr@gmail.com>
2022-12-01 12:02:38 -0500

    style card comp/ hard coded

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,16 @@ import React from "react";
 
 export default function Card() {
     return (
-        <section className="card--component">
-
-        </section>
+        <div className="card--component">
+            <img src="../public/katie-zaferes.png" className="card--photo" />
+            <div className="card--info">
+                <img src="../public/Star.png" className="star--photo" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span>/ person</p>
+        </div>
     )
 }
