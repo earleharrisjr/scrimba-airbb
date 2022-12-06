afbad872a5b3cb18dc1f885f75f7670587d07810
Earl Harris <earleharrisjr@gmail.com>
2022-12-06 13:57:37 -0500

    add props to display card

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,15 @@ function App() {
     <div>
       <NavBar />
       <Hero />
-      <Card />
+      <Card
+        image="katie-zaferes.png"
+        rating={5.0}
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price="From $136"
+
+      />
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card--component">
-            <img src="../public/katie-zaferes.png" className="card--photo" />
+            <img src={`../public/${props.image}`} className="card--photo" />
+            {/* above is string interpolation used with JSX. Notice how we use brackets, then the tiks, then the ${}. */}
             <div className="card--info">
                 <img src="../public/Star.png" className="star--photo" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span>/ person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">{props.price}</span>/ person</p>
         </div>
     )
 }
