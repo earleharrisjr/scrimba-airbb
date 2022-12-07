a9e556065f4a23fde838a57e439812ddaf910f36
Earl Harris <earleharrisjr@gmail.com>
2022-12-07 10:39:03 -0500

    add section for cards. style with CSS for flex

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import './App.css'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import cardData from './cardData'
 
 function App() {
+  const cardElements = cardData.map(card => {
+    return <Card
+      image={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      location={card.location}
+      title={card.title}
+      price={card.price}
+    />
 
+
+  })
   return (
     <div>
       <NavBar />
       <Hero />
-      <Card
-        image="katie-zaferes.png"
-        rating={5.0}
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price="From $136"
-
-      />
+      <section className='cards-list'>
+        {cardElements}
+      </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 
+
+
 export default function Card(props) {
+
+
     return (
-        <div className="card--component">
+        <div className="card">
             <img src={`../public/images/${props.image}`} className="card--photo" />
             {/* above is string interpolation used with JSX. Notice how we use brackets, then the tiks, then the ${}. */}
             <div className="card--info">
                 <img src="../public/images/Star.png" className="star--photo" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">{props.price}</span>/ person</p>
+            <p className="card--title">{props.title}</p>
+            <p><span className="card--price">{props.price}</span>/ person</p>
         </div>
     )
 }
