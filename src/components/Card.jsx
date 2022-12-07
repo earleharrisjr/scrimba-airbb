34a343daac5f8b31dcfd2a4c91d5bccdcbc55942
Earl Harris <earleharrisjr@gmail.com>
2022-12-07 09:59:42 -0500

    created images folder in public

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function Card(props) {
     return (
         <div className="card--component">
-            <img src={`../public/${props.image}`} className="card--photo" />
+            <img src={`../public/images/${props.image}`} className="card--photo" />
             {/* above is string interpolation used with JSX. Notice how we use brackets, then the tiks, then the ${}. */}
             <div className="card--info">
-                <img src="../public/Star.png" className="star--photo" />
+                <img src="../public/images/Star.png" className="star--photo" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
                 <span className="gray">{props.country}</span>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NavBar() {
     return (
         <nav>
-            <img src="public/airbnbLarge.png" className="nav--icon" />
+            <img src="public/images/airbnbLarge.png" className="nav--icon" />
         </nav>
     )
 }
